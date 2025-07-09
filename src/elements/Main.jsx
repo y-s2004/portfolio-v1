6202259e1e4c27c1801bee24e2e29d6d76092912
Y.S <saitou.yoshiki.8@gmail.com>
2025-07-09 18:27:47 +0900

    Summary

--- src/elements/Main.jsx
+++ src/elements/Main.jsx
@@ -6,7 +6,7 @@ import Contact from './Contact';
 
 function Main() {
     return (
-        <BrowserRouter>
+        <BrowserRouter basename='/portfolio-v1'>
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
